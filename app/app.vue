1080e0bc7f1fd86d9a70f1272d0e7dca368b8e94
pycode <devpython549@gmail.com>
2025-12-17 12:35:42 +0530

    initilization

---
+++ app/app.vue
@@ -0,0 +1,39 @@
+<script setup>
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', href: 'https://staginghr.excellencetechnologies.in/exce_favicon.png' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'Excellence HRMS'
+const description = 'Human Resource Management System'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: 'https://staginghr.excellencetechnologies.in/exce_favicon.png',
+  twitterImage: 'https://staginghr.excellencetechnologies.in/exce_favicon.png',
+  twitterCard: 'Excellence HRMS'
+})
+</script>
+
+<template>
+  <UApp>
+    <UHeader title="HRMS">
+    </UHeader>
+
+    <UMain>
+      <NuxtPage />
+    </UMain>
+    <UFooter>
+    </UFooter>
+  </UApp>
+</template>
